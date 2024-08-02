88f9b1d942b0e70b77f6f78afb2543d2fff1705a
goriiin <afk00.17@bk.ru>
2024-08-03 00:50:47 +0300

    login req

--- web/back/app/views.py
+++ web/back/app/views.py
@@ -39,42 +39,42 @@ def log_in(request):
         return JsonResponse({'csrf_token': csrf_token})
 
 
-# import numpy as np
-# import torch
-# import tritonclient.grpc as grpcclient
-# from PIL import Image
-# import datetime
-# from django.utils import timezone
-#
-# @login_required(login_url='login')
-# def handle_model(request):
-#     file = request.FILES.get('img')
-#     if not file:
-#         return JsonResponse({'status': 'error', 'message': 'No image uploaded'})
-#     triton_client = grpcclient.InferenceServerClient(
-#         url="0.0.0.0:8001"
-#     )
-#
-#     img = Image.open(file)
-#     input_data = img.cpu().numpy().astype(np.float32)
-#     input_data = input_data.reshape([-1] + list(input_data.shape))
-#     inputs = [grpcclient.InferInput("keras_tensor", input_data.shape, "FP32")]
-#     inputs[0].set_data_from_numpy(input_data)
-#     outputs = [
-#         grpcclient.InferRequestedOutput("output_0"),
-#     ]
-#
-#     result = triton_client.infer(
-#         model_name="resnet18", inputs=inputs, outputs=outputs
-#     )
-#     out = result.as_numpy()
-#     print(out)
-#
-#     md = ModifiedData.objects.create(data=out.tolist(), date=timezone.now())
-#     md.save()
-#     res = {"result": out.tolist()}
-#
-#     return res
+import numpy as np
+import torch
+import tritonclient.grpc as grpcclient
+from PIL import Image
+import datetime
+from django.utils import timezone
+
+@login_required(login_url='login')
+def handle_model(request):
+    file = request.FILES.get('img')
+    if not file:
+        return JsonResponse({'status': 'error', 'message': 'No image uploaded'})
+    triton_client = grpcclient.InferenceServerClient(
+        url="0.0.0.0:8001"
+    )
+
+    img = Image.open(file)
+    input_data = img.cpu().numpy().astype(np.float32)
+    input_data = input_data.reshape([-1] + list(input_data.shape))
+    inputs = [grpcclient.InferInput("keras_tensor", input_data.shape, "FP32")]
+    inputs[0].set_data_from_numpy(input_data)
+    outputs = [
+        grpcclient.InferRequestedOutput("output_0"),
+    ]
+
+    result = triton_client.infer(
+        model_name="resnet18", inputs=inputs, outputs=outputs
+    )
+    out = result.as_numpy()
+    print(out)
+
+    md = ModifiedData.objects.create(data=out.tolist(), date=timezone.now())
+    md.save()
+    res = {"result": out.tolist()}
+
+    return res
 
 
 def log_out(request):
