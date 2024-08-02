0e0f5701587351c9f20bf4df6178b960446b7b90
goriiin <afk00.17@bk.ru>
2024-08-02 19:37:42 +0300

    handler

--- web/back/app/management/commands/handler_test.py
+++ web/back/app/management/commands/handler_test.py
@@ -3,26 +3,27 @@ import numpy as np
 import torch
 import tritonclient.grpc as grpcclient
 
-
-def model_handler():
-    triton_client = grpcclient.InferenceServerClient(
-        url="0.0.0.0:8001"
-    )
-
-    # img = (torch.rand(128, 336, 1) * 255)
-    img = ModifiedData.objects.get(pk=1)
-    input_data = img.numpy().astype(np.float32)
-    input_data = input_data.reshape([-1] + list(input_data.shape))
-    inputs = [grpcclient.InferInput("keras_tensor", input_data.shape, "FP32")]
-    inputs[0].set_data_from_numpy(input_data)
-    outputs = [
-        grpcclient.InferRequestedOutput("output_0"),
-    ]
-
-    result = triton_client.infer(
-        model_name="resnet18", inputs=inputs, outputs=outputs
-    )
-    print(result.as_numpy("output_0"))
+from django.core.management.base import BaseCommand
+from back.settings import BASE_DIR
 
 
-model_handler()
+class Command(BaseCommand):
+    def handler_test(self, *args, **options):
+        triton_client = grpcclient.InferenceServerClient(
+            url="0.0.0.0:8001"
+        )
+
+        # img = (torch.rand(128, 336, 1) * 255)
+        img = ModifiedData.objects.get(pk=1)
+        input_data = img.numpy().astype(np.float32)
+        input_data = input_data.reshape([-1] + list(input_data.shape))
+        inputs = [grpcclient.InferInput("keras_tensor", input_data.shape, "FP32")]
+        inputs[0].set_data_from_numpy(input_data)
+        outputs = [
+            grpcclient.InferRequestedOutput("output_0"),
+        ]
+
+        result = triton_client.infer(
+            model_name="resnet18", inputs=inputs, outputs=outputs
+        )
+        print(result.as_numpy("output_0"))
