06da7cd6e148dbf3c1ba14e0a254f35629f411fe
goriiin <afk00.17@bk.ru>
2024-08-06 17:57:09 +0300

    test handler

--- web/back/app/management/commands/handler_test.py
+++ web/back/app/management/commands/handler_test.py
@@ -1,21 +1,39 @@
+from tkinter import Image
+
 from app.models import *
 import numpy as np
 import torch
 import tritonclient.grpc as grpcclient
-
+import os
 from django.core.management.base import BaseCommand
 from back.settings import BASE_DIR
 
-
 class Command(BaseCommand):
+    help = 'Тестирование ручки тритона'
+
+    def add_arguments(self, parser):
+        parser.add_argument('type', type=int, help='тип тестирования\n'
+                                                   '\trandom - генерация тензора\n'
+                                                   '\tpic - берем файл из тестовых данных\n')
+
     def handle(self, *args, **options):
         triton_client = grpcclient.InferenceServerClient(
             url="0.0.0.0:8001"
         )
 
-        img = ModifiedData.objects.get(pk=1)
+        if options['type'] == 'random':
+            img = (torch.rand(128, 336, 1)*255)
+            input_data = img.cpu().numpy()
+        elif options['type'] == 'pic':
+            filepath = os.path.join(BASE_DIR, "uploads_dataset", "df_diff2.csv")
+            img = Image.open(filepath)
 
-        input_data = np.array(img.data, dtype=np.float32)
+
+        else:
+            print('Неизвестный тип тестирования')
+            return
+
+
 
         input_data = input_data.reshape([-1] + list(input_data.shape))
 
