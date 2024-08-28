6c5daf708e75cc00ca794e7bfac847927c5f64cb
goriiin <afk00.17@bk.ru>
2024-08-29 00:54:22 +0300

    model

--- web/back/app/views.py
+++ web/back/app/views.py
@@ -197,7 +197,8 @@ def handle_model(request):
             grayscale_image_data[row, col] = color_to_grayscale(image_data[row, col])
 
     grayscale_image = Image.fromarray(grayscale_image_data, 'L')
-    img = grayscale_image.convert
+    img = grayscale_image.convert('L')  # Правильное преобразование в изображение в градациях серого
+    img = img.resize((336, 128))
     # img = img.resize((336, 128))  # изменение размера изображения
     input_data = np.array(img).astype(np.float32) / 255.0 * 2 - 1  # нормализация как в обучении
     input_data = input_data.reshape(128, 336, 1)
