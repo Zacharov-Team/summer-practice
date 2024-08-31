b8d966349f6de170ad11f0a8bd8b274d57b36c39
goriiin <afk00.17@bk.ru>
2024-08-31 17:28:29 +0300

    создание юзера со своими данными

--- web/back/app/management/commands/make_admin.py
+++ web/back/app/management/commands/make_admin.py
@@ -1,24 +1,19 @@
-from app.models import *
-import csv
-from django.db.utils import IntegrityError
-import os
+
 from django.core.management.base import BaseCommand
-from back.settings import BASE_DIR
+
 
 from django.contrib.auth.models import User
 from django.utils import timezone
-from environs import Env
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        env = Env()
-        env.read_env()
         user = User.objects.create_user(
-            username=env('DB_USER'),
-            password=env('DB_PASSWORD'),
+            username=input('введите имя пользователя: '),
+            password=input('введите пароль: '),
             last_login=timezone.now(),
             is_superuser=True,
             is_staff=True
         )
+
         user.save()
